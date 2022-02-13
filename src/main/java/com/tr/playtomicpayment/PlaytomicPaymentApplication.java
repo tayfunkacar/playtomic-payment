0d76736c21919b0aa67e03f705383f0fab97862f
Tayfun Kaçar <tayfun.kacar@prometbilisim.com>
2022-02-13 16:30:01 +0300

    playtomic first commit

---
+++ src/main/java/com/tr/playtomicpayment/PlaytomicPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.tr.playtomicpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaytomicPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaytomicPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tr/playtomicpayment/PlaytomicPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tr.playtomicpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaytomicPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
